c3de06f5b861365172baf1cc4c4ac4f3417065a7
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-03-26 17:02:57 +0100

    more

--- src/Controller/ProgrammeEpisodes/RecipesController.php
+++ src/Controller/ProgrammeEpisodes/RecipesController.php
@@ -6,17 +6,18 @@ namespace App\Controller\ProgrammeEpisodes;
 use App\ExternalApi\Recipes\Domain\RecipesApiResult;
 use App\ExternalApi\Recipes\Service\RecipesService;
 use BBC\ProgrammesPagesService\Domain\Entity\Episode;
+use BBC\ProgrammesPagesService\Domain\Entity\ProgrammeContainer;
 
 class RecipesController extends BaseProgrammeEpisodesController
 {
     public function __invoke(
         RecipesService $recipesService,
-        Episode $episode
+        Episode $programme
     ) {
         /** @var RecipesApiResult $recipes */
-        $recipes = $recipesService->fetchRecipesByPid((string) $episode->getPid())->wait();
+        $recipes = $recipesService->fetchRecipesByPid((string) $programme->getPid())->wait();
 
-        return $this->renderWithChrome('programme_episodes/player.html.twig', [
+        return $this->renderWithChrome('programme_episodes/recipes.html.twig', [
             'recipes' => $recipes,
         ]);
     }
